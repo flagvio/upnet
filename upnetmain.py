45b16ed5166c21413b4204053392576a70b810db
dortu-grassi <79800879+dortu-grassi@users.noreply.github.com>
2021-03-01 19:10:17 +0100

    Add files via upload

--- upnetmain.py
+++ upnetmain.py
@@ -5,7 +5,7 @@ from gi.repository import Gtk
 
 class FMain(Gtk.Window):
 	def __init__(self):
-		super(FMain, self).__init__()
+		super(FMain, self).__init__(title="Aggiorna in rete")
 		#self = Gtk.Window()
 		self.set_default_size(800, 500)
 		self.set_position(Gtk.WindowPosition.CENTER)
@@ -13,16 +13,83 @@ class FMain(Gtk.Window):
 		#icona
 		self.set_icon_from_file("rete.png")
 		
+		hbox = Gtk.Box(spacing=1)
+		hbox.set_homogeneous(False)
+		vbox=self.corpo_sx()
+		hbox.pack_start(vbox, True, True, 20)
+		vbox=self.corpo_dx()
+		hbox.pack_start(vbox, True, True, 20)
+		
+		
+		self.add(hbox)        
+	def corpo_dx(self):
+		vbox= Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=1)
+		vbox.set_homogeneous(False)
+		
 		label = Gtk.Label()
-		label.set_text("This is a left-justified label.\nWith multiple lines.")
-        #label.set_justify(Gtk.Justification.LEFT)
-        #self.add(label)
-	
-	#def corpo(self):
+		label.set_markup("<b>FILE DA TRASFERIRE</b>")
+		label.set_justify(Gtk.Justification.LEFT)
+		vbox.pack_start(label, False, True, 0)
+		
+		entry = Gtk.Entry()
+		entry.set_text("                  ")
+		entry.set_editable(False)
+		vbox.pack_start(entry, False, True, 0)
+
+		bt = Gtk.Button.new_with_label("avvia")
+		bt.connect("clicked", self.btClickAvvia)
+		vbox.pack_start(bt, False, False, 10)
+		
+		return vbox
+		
+	def corpo_sx(self):
+		vbox= Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=1)
+		vbox.set_homogeneous(False)
+		
+		label = Gtk.Label()
+		label.set_markup("<b>COMPUTER DA AGGIORNARE</b>")
+		label.set_justify(Gtk.Justification.LEFT)
+		vbox.pack_start(label, False, True, 0)
 		
 		
+		lstPC=self.listaPC()
+		vbox.pack_start(lstPC, True, True, 0)
+		
+		btAggiungi = Gtk.Button.new_with_label("AGGIUNGI")
+		btAggiungi.connect("clicked", self.btClickAggiungi)
+		vbox.pack_start(btAggiungi, False, False, 10)
+
+		return vbox
+		
+	def listaPC(self):
+		listbox = Gtk.ListBox()
+		
+		row = Gtk.ListBoxRow()
+		hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+		label1 = Gtk.Label(label="Automatic Date & Time", xalign=0)
+		label2 = Gtk.Label(label="Requires internet access", xalign=0)
+		hbox.pack_start(label1, True, True, 0)
+		hbox.pack_start(label2, True, True, 0)
+		row.add(hbox)
+		listbox.add(row)
+
+		row = Gtk.ListBoxRow()
+		hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+		label1 = Gtk.Label(label="bau", xalign=0)
+		label2 = Gtk.Label(label="miao", xalign=0)
+		hbox.pack_start(label1, True, True, 0)
+		hbox.pack_start(label2, True, True, 0)
+		row.add(hbox)
+		listbox.add(row)
+		
+		
+		return listbox
+	def btClickAggiungi(self,button):
+		print("Hai cliccato AGGIUNGI")
+	def btClickAvvia(self,button):
+		print("Hai cliccato AVVIA")
 	def main(self):
-		self.show()
+		self.show_all()
 		Gtk.main()
 #if __name__ == "__main__":
 upnet = FMain()
